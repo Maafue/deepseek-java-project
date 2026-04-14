af75f7bb9bebde14ae5516c668f9077e5f532519
Moroz Maksim <morozmaksim99@mail.ru>
2026-04-14 15:00:56 +0300

    Cache

---
+++ src/main/java/by/morozmaksim/deepseekjavaproject/domain/configuration/CacheConfiguration.java
@@ -0,0 +1,31 @@
+package by.morozmaksim.deepseekjavaproject.domain.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+    @Bean
+    public CacheManager cacheManager(){
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeineCacheBuilder());
+        return caffeineCacheManager;
+    }
+
+    @Bean
+    Caffeine<Object, Object> caffeineCacheBuilder(){
+        return Caffeine.newBuilder()
+                .expireAfterWrite(3, TimeUnit.MINUTES);
+    }
+}
